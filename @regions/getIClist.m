b44113ef40e6862dfcfeeeb9be49188fbd94b53f
pietro <pietro.bozzo1998@gmail.com>
2024-10-04 15:13:52 +0200

    getIClist bugfix

--- @regions/getIClist.m
+++ @regions/getIClist.m
@@ -7,8 +7,8 @@ arguments
 end
 
 state_index = this.getIndeces(state);
-list = [];
+list = zeros(size(this.brain_array(state_index).IC_weights));
 for i = 1 : numel(this.brain_array(state_index).IC_weights)
-  list = [list;size(this.brain_array(state_index).IC_weights{i},2)];
+  list(i) = size(this.brain_array(state_index).IC_weights{i},2);
 end
-list(list=0) = 1; % set to 1 as there is one column of NaNs when no ICs are detected
+list(list==0) = 1; % set to 1 as there is one column of NaNs when no ICs are detected
