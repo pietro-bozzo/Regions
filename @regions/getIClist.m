72d5be4859887b67dbc779e60d475b5db3d324e9
pietro <pietro.bozzo1998@gmail.com>
2024-10-04 11:15:00 +0200

    getIClist creation

---
+++ @regions/getIClist.m
@@ -0,0 +1,13 @@
+function list = getIClist(this,state)
+% getIClist Get array of number of ICs per region, computed over a given state
+
+arguments
+  this (1,1) regions
+  state (1,1) string
+end
+
+state_index = this.getIndeces(state);
+list = [];
+for i = 1 : numel(this.brain_array(state_index).IC_weights)
+  list = [list;size(this.brain_array(state_index).IC_weights{i},2)];
+end
