035fc13a7b275771b0d458d6e8d9a63142d33886
hugo <hugo.degeneve@student-cs.fr>
2024-10-21 15:09:20 +0200

    pc reconstruction from spikes count matrix

--- @brain/computeAvalanches.m
+++ @brain/computeAvalanches.m
@@ -3,7 +3,7 @@ function this = computeAvalanches(this,opt)
 
 arguments
   this (1,1) brain
-  opt.threshold (1,1) double {mustBeNonnegative} = 0
+  opt.threshold (1,1) double {mustBeNonnegative} = 2
 end
 
 this.aval_threshold = opt.threshold;
