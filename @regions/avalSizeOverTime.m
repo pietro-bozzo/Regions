e369ec92a57dbe1c50063a5cd1b03a2def6038f0
Gui-83 <guillaume.bruno@laposte.net>
2026-03-03 12:32:12 +0100

    update state restriction

--- @regions/avalSizeOverTime.m
+++ @regions/avalSizeOverTime.m
@@ -3,7 +3,7 @@ function size_t = avalSizeOverTime(this,state,regs)
 
 arguments
   this (1,1) regions
-  state (1,1) string % NOT IMPLEMENTED
+  state (1,1) string
   regs (1,1) string = []
 end
 
@@ -14,15 +14,32 @@ end
 % find requested state and regions
 [~,~,s_index,r_index] = this.arrayInd(state,regs);
 
+intervals = this.regions_array(r_index).aval_intervals;
+
 % get profiles
 size_t = this.regions_array(r_index).aval_size_t;
 
-% filter by state NOT IMPLEMENTED
+
+% filter by state
+if state ~= "all"
+  ind = false(size(intervals(:,1))); % ind(i) = 1 iff interval(i) is in state
+  for state_interval = this.state.times{s_index}.'
+    ind = ind | intervals(:,1) > state_interval(1) & intervals(:,2) < state_interval(2);
+  end
+  % keep only avalanche intervals in state
+  intervals = intervals(ind,:);
+  size_t = size_t(ind);
+end
+
+% RESTRICT marche aussi MAIS : il est l'équivalent de intervals(:,1) >= state_interval(1) & intervals(:,2) < state_interval(2);
+% au lieu de intervals(:,1) > state_interval(1) & intervals(:,2) < state_interval(2);,
+% et il est plus lent
+
+% filter by state
 %if state ~= "all"
-%  ind = false(size(intervals(:,1))); % ind(i) = 1 iff interval(i) is in state
-%  for state_interval = this.state_stamps{s_index}.'
-%    ind = ind | intervals(:,1) > state_interval(1) & intervals(:,2) < state_interval(2);
-%  end
-%  % keep only avalanche intervals in state
-%  intervals = intervals(ind,:);
+ % [~,ind1] = Restrict(intervals(:,1), this.state.times{s_index});
+  %[~,ind2] = Restrict(intervals(:,2), this.state.times{s_index});
+  %ind = intersect(ind1, ind2);
+  %intervals = intervals(ind,:);
+  %size_t = size_t(ind);
 %end
