287ec52ae053269628dec0ff07e440b754ae6e01
pietro <pietro.bozzo1998@gmail.com>
2026-03-03 16:56:52 +0100

    bugfix + make help

--- @regions/avalProfiles.m
+++ @regions/avalProfiles.m
@@ -1,45 +1,44 @@
 function [profiles,time] = avalProfiles(this,state,regs)
 % avalProfiles Get activity profiles used to compute avalanches
+%
+% arguments:
+%     state       string, behavioral state
+%     region      (n,1) string, brain region
+%
+% output:
+%     profiles    (m,n) double, activity profiles over time, zeros separate avalanches
+%     time        (m,1) double, time stamps for profiles
+
+% Copyright (C) 2025 by Pietro Bozzo
+%
+% This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
+% as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
 
 arguments
   this (1,1) regions
-  state (1,1) string % NOT IMPLEMENTED
+  state (1,1) string
   regs (:,1) string = []
 end
 
 if ~this.hasAvalanches()
-  error('avalProfiles:MissingAvalanches','Avalanches have not been computed.')
+  error('avalProfiles:MissingAvalanches','Avalanches have not been computed')
 end
 
-% find requested state and regions
+% find state and regions
 [~,~,s_index,r_indeces] = this.arrayInd(state,regs);
-%intervals = this.regions_array(r_index).aval_intervals;
 
 % get profiles
 profiles = [];
 for r = r_indeces
   profiles = inhomogeneousHorzcat(profiles,this.regions_array(r).aval_profile);
 end
-
-% if requested, output time for profiles
 time = (this.aval_t0 : this.aval_window : this.aval_t0 + this.aval_window * (size(profiles,1)-1)).';
-
-% filter by state NOT IMPLEMENTED
-%if state ~= "all"
-%  ind = false(size(intervals(:,1))); % ind(i) = 1 iff interval(i) is in state
-%  for state_interval = this.state_stamps{s_index}.'
-%    ind = ind | intervals(:,1) > state_interval(1) & intervals(:,2) < state_interval(2);
-%  end
-%  % keep only avalanche intervals in state
-%  intervals = intervals(ind,:);
-%end
-
 
 % filter by state
 if state ~= "all"
-  ind = false(size(time(:,1))); % ind(i) = 1 iff interval(i) is in state
+  ind = false(size(time(:,1))); % ind(i) = 1 iff time(i) is in state
   for state_interval = this.state.times{s_index}.'
-    ind = ind | time > state_interval(1) & time < state_interval(2);
+    ind = ind | time >= state_interval(1) & time <= state_interval(2);
   end
   time = time(ind);
   profiles = profiles(ind);
--- @regions/avalSizeOverTime.m
+++ @regions/avalSizeOverTime.m
@@ -1,45 +1,56 @@
-function size_t = avalSizeOverTime(this,state,regs)
-% avalProfiles Get avalanche size over time
+function [size_t,time] = avalSizeOverTime(this,state,reg)
+% avalSizeOverTime Get avalanche size over time
+%
+% arguments:
+%     state     string, behavioral state
+%     region    string, brain region
+%
+% output:
+%     size_t    (n,1) double, avalanche size over time, each 0 represents the beginning of an avalanche
+%     time      (n,1) double, time stamps for size_t
+
+% Copyright (C) 2025 by Pietro Bozzo
+%
+% This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
+% as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
 
 arguments
   this (1,1) regions
   state (1,1) string
-  regs (1,1) string = []
+  reg (1,1) string = []
 end
 
 if ~this.hasAvalanches()
-  error('avalProfiles:MissingAvalanches','Avalanches have not been computed')
+  error('avalSizeOverTime:MissingAvalanches','Avalanches have not been computed')
 end
 
-% find requested state and regions
-[~,~,s_index,r_index] = this.arrayInd(state,regs);
+% find state and region
+[~,~,s_index,r_index] = this.arrayInd(state,reg);
 
-intervals = this.regions_array(r_index).aval_intervals;
-
-% get profiles
+% get profile
 size_t = this.regions_array(r_index).aval_size_t;
-
+if size_t(1) ~= 0
+  size_t = [0;size_t];
+end
+intervals = this.regions_array(r_index).aval_intervals;
 
 % filter by state
 if state ~= "all"
+  % find avalanches inside state
   ind = false(size(intervals(:,1))); % ind(i) = 1 iff interval(i) is in state
   for state_interval = this.state.times{s_index}.'
-    ind = ind | intervals(:,1) > state_interval(1) & intervals(:,2) < state_interval(2);
+    ind = ind | intervals(:,1) >= state_interval(1) & intervals(:,2) <= state_interval(2);
   end
-  % keep only avalanche intervals in state
   intervals = intervals(ind,:);
-  size_t = size_t(ind);
+  % separate avalanches in size_t
+  aval_idx = find(~size_t);
+  aval_idx = [aval_idx,[aval_idx(2:end)-1;numel(size_t)]]; % aval_idx(i,:) is [start, stop] indeces of i-th avalanche
+  % keep only avalanches inside state
+  aval_idx = aval_idx(ind,:);
+  keep_idx = linspaceVector(aval_idx(:,1),aval_idx(:,2));
+  size_t = size_t(keep_idx);
 end
 
-% RESTRICT marche aussi MAIS : il est l'équivalent de intervals(:,1) >= state_interval(1) & intervals(:,2) < state_interval(2);
-% au lieu de intervals(:,1) > state_interval(1) & intervals(:,2) < state_interval(2);,
-% et il est plus lent
-
-% filter by state
-%if state ~= "all"
- % [~,ind1] = Restrict(intervals(:,1), this.state.times{s_index});
-  %[~,ind2] = Restrict(intervals(:,2), this.state.times{s_index});
-  %ind = intersect(ind1, ind2);
-  %intervals = intervals(ind,:);
-  %size_t = size_t(ind);
-%end
+if nargout > 1
+  time = linspaceVector(intervals(:,1),intervals(:,2),this.aval_window);
+end
